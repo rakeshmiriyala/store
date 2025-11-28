6890e8bfd40a0350daef5ecb4577c9cb6b48d672
rakeshmiriyala <rakeshmiriyala74@gmail.com>
2025-11-28 08:08:22 +0530

    server changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   server: {
-    host: true,           // ✅ allows external access
-    port: 5173,           // optional, custom port
-    allowedHosts: ["store-oxsn.onrender.com","store-qk5w.onrender.com"], // ✅ optional in Render
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      "store-oxsn.onrender.com",
+      "store-qk5w.onrender.com",
+    ],
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
